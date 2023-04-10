42c7ff1f96155ef23975dbf7a59dc31dc78c9be0
taehee <jangth0655@gmail.com>
2023-04-10 21:40:15 +0900

    feat : 캐러셀 포스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import CarouselPosts from '@/components/CarouselPosts';
+import FeaturedPosts from '@/components/FeaturedPost';
 import Hero from '@/components/Hero';
+import MultiCarousel from '@/components/MultiCarousel';
 
 export default function HomePage() {
   return (
     <section>
       <Hero />
+      {/* @ts-expect-error Async Server Component */}
+      <FeaturedPosts />
+
+      {/* @ts-expect-error Async Server Component */}
+      <CarouselPosts />
     </section>
   );
 }
